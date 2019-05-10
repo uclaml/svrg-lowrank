f8c53bbe976d0d2dfb4a884cc4a7d7ef19271abf
leowang7267 <lingxiao7ustb@gmail.com>
2019-05-10 13:54:34 -0700

    Update data_generation_mc.m

--- data_generation_mc.m
+++ data_generation_mc.m
@@ -1,8 +1,8 @@
-function [M_star] = data_generation_mc(d1,r)
+function [M_star] = data_generation_mc(d1,d2,r)
 
 % Generate M* 
  U_star = randn(d1,r) / sqrt(d1); 
- %V_star = randn(d2,r) / sqrt(d2);
-M_star = U_star*U_star';
+ V_star = randn(d2,r) / sqrt(d2);
+M_star = U_star*V_star';
 end
 
